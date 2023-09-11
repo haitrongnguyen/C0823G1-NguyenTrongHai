ea70f8323eefc332ac98c115128e35b6a4f3de2b
haitrongnguyen <tronghaispt@gmail.com>
2023-09-11 19:36:28 +0700

    nop bai thuc hanh 1 2  cua bai 7

---
+++ ss7_bien_toantu/thuc_hanh/bai_2/bai1.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt("nhập vào chiều rộng")
+inputheight = prompt("Nhập vào chiều cao")
+let width = parseInt(inputwidth)
+let height = parseInt(inputheight)
+let dt = width*height
+document.write("Diện tích là: "+dt)
